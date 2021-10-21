47ef08e49789e680cb2f553d3155972c6e29fa40
Jonathan Gregg <jpgregg@utexas.edu>
2021-10-21 12:02:19 -0700

    Testing a second commit

--- app/src/main/java/com/example/workout_v2/HomeActivity.java
+++ app/src/main/java/com/example/workout_v2/HomeActivity.java
@@ -13,7 +13,7 @@ public class HomeActivity extends AppCompatActivity {
 
         startActivity(intent);
     }
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
